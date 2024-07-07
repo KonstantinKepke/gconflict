206b46ad4069691f08e73bc3338fb55bf21189b7
KonstantinKepke <konstantin.kepke@gmail.com>
2024-07-07 12:32:02 +0300

    Add hello Vasya

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 
 int main (){
-    std::cout << "1 Hello CPP============" << std::endl;
+    std::cout << "1 Hello Vasya !============" << std::endl;
 
     std::cout << "2 Hello CPP============" << std::endl;
     
