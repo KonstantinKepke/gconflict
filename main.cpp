5a990b6189b2055e496cb1c2c8d86402391555e3
KonstantinKepke <konstantin.kepke@gmail.com>
2024-07-07 12:23:19 +0300

    main
    Ref: TEL-0001

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+
+int main (){
+    std::cout << "1 Hello CPP============" << std::endl;
+
+    std::cout << "2 Hello CPP============" << std::endl;
+    
+    std::cout << "3 Hello CPP============" << std::endl;
+    
+    printf("End Cpp !\n");
+    return 0;
+}
+
+
